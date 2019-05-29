f09f4a0dc8087c1ed1fbe2fb6f73e05598270803
Bradley Garrod <bradleybredgarrod@gmail.com>
2019-05-29 23:03:15 +0100

    Added C solution 4

---
+++ language-proficiency/4-functions-in-c/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d);
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int m1 = a > b ? a : b;
+    int m2 = c > d ? c : d;
+    return m1 > m2 ? m1 : m2;
+}
